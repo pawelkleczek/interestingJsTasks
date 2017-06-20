a57336574f29d76529972e41b18a47a29006fe6d
pawelkleczek <pa.vel.kleczek@gmail.com>
2017-06-20 20:32:14 +0200

    New task

--- js/app.js
+++ js/app.js
@@ -112,15 +112,6 @@ console.log(message);
 //
 // Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
 
-function sym(arg) {
-  	var args = [].slice.call(arguments);
-	let newArray = [];
-	args.forEach( e => newArray.push(e));
-	console.log(newArray);
-}
-
-sym([1, 2, 3], [5, 2, 1, 4]);
-
 function symDif(something) {
 
 	let args = [].slice.call(arguments);
@@ -148,3 +139,44 @@ function symDif(something) {
 }
 
 console.log(symDif([1, 2, 3], [5, 2, 1, 4], [4, 3, 2]));
+
+var c = [1,1,2,2,2,3,4,4,4,4,4,4,5];
+var g = c.filter((e, i, a) => i === a.indexOf(e));
+console.log(g);
+
+/*
+Exact Change
+
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+Return the string "Insufficient Funds" if cash-in-drawer is less than the change due. Return the string "Closed" if cash-in-drawer is equal to the change due.
+
+Otherwise, return change in coin and bills, sorted in highest to lowest order.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Global Object
+*/
+
+function checkCashRegister(price, cash, cid) {
+  var change;
+  // Here is your change, ma'am.
+  return change;
+}
+
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.10],
+// ["QUARTER", 4.25],
+// ["ONE", 90.00],
+// ["FIVE", 55.00],
+// ["TEN", 20.00],
+// ["TWENTY", 60.00],
+// ["ONE HUNDRED", 100.00]]
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
